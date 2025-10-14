b85a4b3984698a6b0d0aa2793d4415b9a067fac4
quick007 <69743171+quick007@users.noreply.github.com>
2025-10-14 16:27:25 -0700

    chore: unify package management to pipfile

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -2,8 +2,9 @@
 set -e
 
 if [[ ${APP_ENVIRONMENT} == "development" ]]; then
-    ${APP_ENV}/bin/pip install -r /app/docker/requirements-dev.txt
+    echo "Starting development server"
     ${APP_ENV}/bin/gunicorn -b 0.0.0.0:8000 --reload oregoninvasiveshotline.wsgi
 else
+    echo "Starting production server"
     ${APP_ENV}/bin/gunicorn -b 0.0.0.0:8000 oregoninvasiveshotline.wsgi
 fi
