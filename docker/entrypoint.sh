4e8b9f170d07748f4116034f0f64f5874ef4971c
Lance Albertson <lance@osuosl.org>
2025-10-27 13:39:40 -0700

    Remove uwsgi
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,12 +9,7 @@ if [[ ${APP_SERVICE} == "wsgi" ]]; then
     # Run migrations and collect static files before starting the app
     ${APP_ENV}/bin/python manage.py migrate --no-input
     ${APP_ENV}/bin/python manage.py collectstatic --no-input
-
-    if [[ ${DJANGO_ENV} == "docker" ]]; then
-        ${APP_ENV}/bin/gunicorn -b 0.0.0.0:8000 --reload oregoninvasiveshotline.wsgi
-    else
-        exec ${APP_ENV}/bin/uwsgi --include /uwsgi/uwsgi.ini
-    fi
+    ${APP_ENV}/bin/gunicorn -b 0.0.0.0:8000 --reload oregoninvasiveshotline.wsgi
 elif [[ ${APP_SERVICE} == "celery" ]]; then
     exec ${APP_ENV}/bin/celery -A oregoninvasiveshotline worker -l INFO
 elif [[ ${APP_SERVICE} == "scheduler" ]]; then
