4fc7b3b2ae176857c66e02ece665888de33a518d
Lance Albertson <lance@osuosl.org>
2025-11-03 10:58:14 -0800

    Use consistent quotation styles
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -15,7 +15,7 @@ if [[ ${APP_SERVICE} == "wsgi" ]]; then
       ${APP_ENV}/bin/gunicorn \
         --access-logfile '-' \
         --error-logfile '-' \
-        --log-file "-" \
+        --log-file '-' \
         --access-logformat '%({x-forwarded-for}i)s %(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"' \
         --forwarded-allow-ips="${LOAD_BALANCER_IPS:-127.0.0.1}" \
         -w 4 \
