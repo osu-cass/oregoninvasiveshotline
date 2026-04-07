fe67ff6efe249b7230ec3ab1bd619a89598121a6
quick007 <69743171+quick007@users.noreply.github.com>
2026-04-07 14:28:20 -0700

    chore: attempt to fix playwright tests

--- tests/shared/infra.ts
+++ tests/shared/infra.ts
@@ -9,11 +9,11 @@ export const INFRA_ENV: Record<string, string> = {
 		"./volumes/playwright",
 	USER_ID: process.env.USER_ID || "1000",
 	GROUP_ID: process.env.GROUP_ID || "1000",
-	APP_PORT: process.env.APP_PORT || process.env.PLAYWRIGHT_APP_PORT || "8001",
+	APP_PORT: process.env.APP_PORT || process.env.PLAYWRIGHT_APP_PORT || "8000",
 	VITE_PORT:
-		process.env.VITE_PORT || process.env.PLAYWRIGHT_VITE_PORT || "5174",
+		process.env.VITE_PORT || process.env.PLAYWRIGHT_VITE_PORT || "5173",
 	POSTGRES_PORT:
-		process.env.POSTGRES_PORT || process.env.PLAYWRIGHT_POSTGRES_PORT || "5440",
+		process.env.POSTGRES_PORT || process.env.PLAYWRIGHT_POSTGRES_PORT || "5439",
 	DEBUG_PORT:
-		process.env.DEBUG_PORT || process.env.PLAYWRIGHT_DEBUG_PORT || "1081",
+		process.env.DEBUG_PORT || process.env.PLAYWRIGHT_DEBUG_PORT || "1080",
 };
