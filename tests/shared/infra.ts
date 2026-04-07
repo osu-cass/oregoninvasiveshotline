f7189d8abf9d3660b47e752d1abbed947b0c4daa
quick007 <69743171+quick007@users.noreply.github.com>
2026-04-07 15:23:26 -0700

    chore: remove unesseary ports

--- tests/shared/infra.ts
+++ tests/shared/infra.ts
@@ -1,19 +1,10 @@
 export const INFRA_ENV: Record<string, string> = {
-	COMPOSE_PROJECT_NAME:
-		process.env.COMPOSE_PROJECT_NAME ||
-		process.env.PLAYWRIGHT_COMPOSE_PROJECT_NAME ||
-		"playwright",
-	VOLUME_PATH:
-		process.env.VOLUME_PATH ||
-		process.env.PLAYWRIGHT_VOLUME_PATH ||
-		"./volumes/playwright",
+	COMPOSE_PROJECT_NAME: process.env.COMPOSE_PROJECT_NAME || "playwright",
+	VOLUME_PATH: process.env.VOLUME_PATH || "./volumes/playwright",
 	USER_ID: process.env.USER_ID || "1000",
 	GROUP_ID: process.env.GROUP_ID || "1000",
-	APP_PORT: process.env.APP_PORT || process.env.PLAYWRIGHT_APP_PORT || "8001",
-	VITE_PORT:
-		process.env.VITE_PORT || process.env.PLAYWRIGHT_VITE_PORT || "5174",
-	POSTGRES_PORT:
-		process.env.POSTGRES_PORT || process.env.PLAYWRIGHT_POSTGRES_PORT || "5440",
-	DEBUG_PORT:
-		process.env.DEBUG_PORT || process.env.PLAYWRIGHT_DEBUG_PORT || "1081",
+	APP_PORT: process.env.APP_PORT || "8001",
+	VITE_PORT: process.env.VITE_PORT || "5174",
+	POSTGRES_PORT: process.env.POSTGRES_PORT || "5440",
+	DEBUG_PORT: process.env.DEBUG_PORT || "1081",
 };
